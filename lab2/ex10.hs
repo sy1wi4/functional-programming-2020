7f816e84862dda3328a38a999bb416099121c641
Sylwia Marek <56658876+sy1wi4@users.noreply.github.com>
2020-11-03 12:55:54 +0100

    added divider3

--- lab2/ex10.hs
+++ lab2/ex10.hs
@@ -5,6 +5,13 @@ fst2Eq (x : y : _) | x == y = True
 fst2Eq _                    = False
 
 
+-- czy pierwszy element listy jest podzielnikiem drugiego
 divider :: (Eq a, Integral a) => [a] -> Bool
 divider (x : y : _) | y `mod` x == 0 = True
 divider _                    = False
+
+
+--  czy pierwszy element listy jest podzielnikiem trzeciego
+divider3 :: (Eq a, Integral a) => [a] -> Bool
+divider3 (x : y : z : _) | z `mod` x == 0 = True
+divider3 _                    = False
