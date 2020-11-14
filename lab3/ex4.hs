da2647b7a3df2d27bd78b0d84f1f7bd309583f8e
Sylwia Marek <56658876+sy1wi4@users.noreply.github.com>
2020-11-14 23:53:09 +0100

    added ex4

---
+++ lab3/ex4.hs
@@ -0,0 +1,16 @@
+-- Funkcje jako elementy struktur danych
+
+
+funcList :: [ Double -> Double ]
+funcList = [ \x -> (sin x)/x, \x -> log x + sqrt x + 1, \x -> (exp 1) ** x ]
+
+
+evalFuncListAt :: a -> [a -> b] -> [b]
+evalFuncListAt x [] = []
+evalFuncListAt x (f:fs) = f x : evalFuncListAt x fs
+
+
+displEqs :: (Double -> Double, Double -> Double)
+displEqs = (\t -> 4 * t^2 + 2 * t, \t -> 3 * t^2)
+-- let (x_t, y_t) = (fst displEqs, snd displEqs)  
+-- pierwszy i drugi element krotki
