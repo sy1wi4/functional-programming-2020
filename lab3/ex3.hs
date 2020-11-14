acbb15b5598c354ed1f179de127b7b731d4d87cf
Sylwia Marek <56658876+sy1wi4@users.noreply.github.com>
2020-11-14 23:42:24 +0100

    added ex3

---
+++ lab3/ex3.hs
@@ -0,0 +1,18 @@
+-- Funkcje wyższego rzędu: funkcje jako wyniki
+
+sqr x = x^2
+
+funcFactory n = case n of
+ 1 -> id
+ 2 -> sqr
+ 3 -> (^3)
+ 4 -> \x -> x^4
+ 5 -> intFunc
+ _ -> const n
+ where
+   intFunc x = x^5
+
+
+expApproxUpTo :: Int -> Double -> Double
+expApproxUpTo n = \x -> sum [x^k/fromIntegral (factorial k) | k <- [0..n]]
+    where factorial k = if k == 0 then 1 else k*factorial (k-1)
