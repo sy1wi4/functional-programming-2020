646f5c26cc88851046567d4fd41ba0a2cfcb7937
Sylwia Marek <56658876+sy1wi4@users.noreply.github.com>
2020-11-16 19:22:54 +0100

    added infix info

--- lab3/ex5.hs
+++ lab3/ex5.hs
@@ -1,5 +1,9 @@
 -- Operator złożenia funkcji (.) (i notacja point-free)
 
+-- infix, infixl, infixr -> priorytet: [0..9]
+
+-- operator (.) - infixr 9 oznacza najwyższy priorytet (łączność prawostronna (r))
+
 
 import Data.List
 sortDesc :: Ord a => [a] -> [a]
@@ -8,6 +12,13 @@ sortDesc xs = reverse . sort $ xs
 
 -- sortDesc' = reverse . sort   ???
 
+
 f = (+1)
+g = (*2) 
 h = (^3)
+
+fg = f.g
+gh = g.h
+fgh = f.g.h
+
 w3 = \x y z -> sqrt (x^2 + y^2 + z^2)
