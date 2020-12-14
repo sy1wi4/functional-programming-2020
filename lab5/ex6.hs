a8591d9ebfb050c14d010488c40373cb81079bd7
Sylwia Marek <56658876+sy1wi4@users.noreply.github.com>
2020-12-14 18:53:32 +0100

    added ex6

---
+++ lab5/ex6.hs
@@ -0,0 +1,16 @@
+{-# LANGUAGE DeriveFunctor #-}
+-- Funktory 2: dołączanie typów użytkownika do klasy Functor
+
+
+newtype Box a = MkBox a deriving (Show, Functor)
+
+-- instance Functor Box where
+--   fmap f (MkBox x) = MkBox (f x)
+
+
+data MyList a = EmptyList
+              | Cons a (MyList a) deriving Show
+
+instance Functor MyList where
+  fmap _ EmptyList    = EmptyList
+  fmap f (Cons x mxs) = Cons (f x) (fmap f mxs)
